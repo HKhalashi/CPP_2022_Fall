9c4ed2f5110e8b82140f1bd5891887f050527987
HKhalashi <86408114+HKhalashi@users.noreply.github.com>
2022-12-15 14:58:34 +0300

    Create F.cpp

---
+++ Test_2/F.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+long long  f(long long a, long long b, long long c)
+{
+    if (a <= 0 || b <= 0 || c <= 0) return 1;
+    else if (a > 20 || b > 20 || c > 20) return f(20, 20, 20);
+    else if (a < b && b < c) return f(a, b - 1, c) + f(a, b, c - 1) - f(a, b - 1, c - 1);
+    else
+    {
+        a--;
+        return f(a, b - 1, c) + f(a, b, c - 1) - f(a, b - 1, c - 1) + f(a, b, c);
+    }
+}
+int main() {
+    long long a , b , c;
+    std::cin>> a>> b >>c;
+    f(a , b , c);
+    return 0;
+}
