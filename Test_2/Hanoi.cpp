96b11eaec613a4ef4bd6b20bca79938528584038
HKhalashi <86408114+HKhalashi@users.noreply.github.com>
2022-12-15 15:18:45 +0300

    Update Hanoi.cpp

--- Test_2/Hanoi.cpp
+++ Test_2/Hanoi.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 
+
 void han(int n, int f, int l) {
-  if (n > 0) {
-    int temp = 6 - f - l;
-    if ((l - f) % 3 == 1) {
-      han(n - 1, f, temp);
-      std::cout << n << " " << f << " " << l << std::endl;
-      han(n - 1, temp, l);
-    } else {
+  int t = 6 - f - l;
+  if (n > 0)
+    if (f % l == f) {
       han(n - 1, f, l);
-      std::cout << n << " " << f << " " << temp << std::endl;
+      std::cout << n << " " << f << " " << t << std::endl;
       han(n - 1, l, f);
-      std::cout << n << " " << temp << " " << l << std::endl;
+      std::cout << n << " " << t << " " << l << std::endl;
       han(n - 1, f, l);
     }
+  else {
+    han(n - 1, f, t);
+    std::cout << n << " " << f << " " << l << std::endl;
+    han(n - 1, t, l);
   }
 
 }
@@ -22,6 +23,5 @@ int main() {
   int n;
   std::cin >> n;
   han(n, 1, 3);
-
   return 0;
 }
